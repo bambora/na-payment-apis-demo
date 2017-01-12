a5e15a2507b0fe8e7f0c8551eb9b682b1c11e049
Aengus Bates <aengus.bates@bambora.com>
2017-01-11 19:20:18 -0800

    Updated deploy scripts

--- payments_api_demo/app/app.py
+++ payments_api_demo/app/app.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def test():
   perams = request.args
-  print('searchword')
-  print(searchword)
   return render_template('hello.html', perams=perams)
 
 if __name__ == '__main__':
